ed6cb8d52dd9e77574d86cb4f6b7b20e572e987d
alice <alice@192.168.39.146>
2019-10-03 15:07:22 -0500

    Controlador1

--- src/main/java/com/udec/cuenta_de_cobro/controlador/Datos.java
+++ src/main/java/com/udec/cuenta_de_cobro/controlador/Datos.java
@@ -1,25 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.udec.cuenta_de_cobro.controlador;
 
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 /**
  *
- * @author alice
+ * @author Alisson Celeita
  */
-@Named(value = "datos")
-@Dependent
+@Named
+@RequestScoped
 public class Datos {
+    private String nombre;
+    private String apellido;
+    private String profesion;
+    private String genero;
+    private String[] idioma;
+    private String ubicacion;
+    private int dias;
 
-    /**
-     * Creates a new instance of Datos
-     */
-    public Datos() {
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String[] getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String[] idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+    
     
 }
