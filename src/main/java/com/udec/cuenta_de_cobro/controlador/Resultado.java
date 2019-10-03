2e0539aeb779277c464ca82b2220d72df5d3e34a
Manrique <angie27manrique@gmail.com>
2019-10-03 15:27:50 -0500

    vista y controlador resultado

---
+++ src/main/java/com/udec/cuenta_de_cobro/controlador/Resultado.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.cuenta_de_cobro.controlador;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author angie
+ */
+@Named(value = "resultado")
+@RequestScoped
+public class Resultado {
+
+   String nombre;
+   String apellido;
+   String profesion;
+   String genero;
+   String[] idioma;
+   String ubicacion;
+   int dias_trabajados;
+    
+    public Resultado() {
+    }
+        
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String[] getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String[] idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getDias_trabajados() {
+        return dias_trabajados;
+    }
+
+    public void setDias_trabajados(int dias_trabajados) {
+        this.dias_trabajados = dias_trabajados;
+    }
+ 
+    
+}
