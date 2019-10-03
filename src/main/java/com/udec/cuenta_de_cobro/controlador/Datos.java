4aa3e426c8cbece1648d260706f6f0a8000935f5
alice <alice@192.168.39.146>
2019-10-03 14:28:55 -0500

    Carpetas

---
+++ src/main/java/com/udec/cuenta_de_cobro/controlador/Datos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.cuenta_de_cobro.controlador;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author alice
+ */
+@Named(value = "datos")
+@Dependent
+public class Datos {
+
+    /**
+     * Creates a new instance of Datos
+     */
+    public Datos() {
+    }
+    
+}
